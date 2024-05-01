a87d70e71df874c49d5a5d295c12ba0070e6f808
MedyWSCW <159732892+MedyWSCW@users.noreply.github.com>
2024-05-02 10:27:19 +1200

    Update Main

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print ("hello")
+
+ # Ask the user their name and store it
+input ("What is your Name ")
+ #Greet the user and introduce the quiz
+print ( "Welcome to this quiz")
+print ( "This quiz is about Movies")
+ #Ask the user a question
+input ( "What is  spidermans real name" )
+ #Tell them the correct answer
+print("the answer is Peter Parker")
+ #End the quiz
+print("Thanks for playing!")
+
