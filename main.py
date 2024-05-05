f1b481b3bfec754296230fb43acd81a842adbf1a
MedyWSCW <159732892+MedyWSCW@users.noreply.github.com>
2024-05-06 11:52:59 +1200

    Changes

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-
- # Ask the user their name and store it
-input ("What is your Name ")
+# Ask the user their name and store it
+name = input ("What is your Name? ")
  #Greet the user and introduce the quiz
-print ( "Welcome to this quiz")
-print ( "This quiz is about Movies")
+print ( "Welcome to this quiz",name)
+print ( "This quiz is about Superheros")
  #Ask the user a question
-input ( "What is  spidermans real name" )
+answer = input ( "What is spidermans real name? " )
+if answer == "Peter Parker":
+ print ("Correct")
+elif answer == "":
+ print ("Not sure?")
+else:
+ print("Incorrect")
  #Tell them the correct answer
-print("the answer is Peter Parker")
+print ("The answer is Peter Parker")
  #End the quiz
-print("Thanks for playing!")
+print ("Thanks for playing!")
 
