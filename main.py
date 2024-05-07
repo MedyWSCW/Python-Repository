1603bedd7dd6f9dd82ab59b95bfbc8985426e22f
MedyWSCW <159732892+MedyWSCW@users.noreply.github.com>
2024-05-07 15:07:03 +1200

    chnages

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ name = input ("What is your Name? ")
 print ( "Welcome to this quiz",name)
 print ( "This quiz is about Superheros")
  #Ask the user a question
-answer = input ( "What is spidermans real name? " ).lower()
-if answer == "Peter Parker".lower():
+question = "What is spidermans real name? ".upper().lower()
+a = "Peter Parker"
+b = "Bruce Banner"
+c = "Tony Stark"
+d = "Steve Rogers"
+answer = input ("{}\nA.{} B.{} C.{} D.{}".format(question, a, b, c, d)).lower()
+if answer == "A" or answer == "a":
  print ("Correct! you got 5 points.")
  score += 5
 elif answer == "":
@@ -16,6 +21,6 @@ else:
  #Tell them the correct answer
 print ("The answer is Peter Parker")
  #End the quiz
-print ("Well done. That's the end. You got ",score, "points")
+print ("Well done {}. That's the end. Your final score is {}".format (name,score ))
 print ("Thanks for playing!")
 
