66f987218916705d6c68d914c26ec5317d5aaf1c
MedyWSCW <159732892+MedyWSCW@users.noreply.github.com>
2024-05-27 11:55:22 +1200

    chnages

---
+++ top10.py
@@ -0,0 +1,20 @@
+import random
+
+score = 0
+MAX_TURNS = 4
+
+# --- FUNCTIONS---
+def intro():
+    print("hello!")
+    
+# --- MAIN CODE---
+intro()
+
+#Welcomes the user and introduce the quiz
+def intro():
+
+    # Ask the user their name 
+    name = input("What is your Name?")
+#Greet the user and introduce the quiz
+    print ("Welcome to this quiz,", name)
+    print ("")
